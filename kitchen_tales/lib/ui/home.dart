4a15f0739574cfb2b6ba3174d0f2d73358d5808e
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-10-18 23:48:11 +0530

    Added Youtube Video

--- kitchen_tales/lib/ui/home.dart
+++ kitchen_tales/lib/ui/home.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class kitchen_tales extends StatelessWidget {
+  YoutubePlayerController _controller = YoutubePlayerController(
+      initialVideoId: '5NrYqZCjpN0',
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,26 +19,29 @@ class kitchen_tales extends StatelessWidget {
         // actions: [
         //   IconButton(icon: Icon(Icons.add_a_photo_sharp), onPressed: null)
         // ],
-
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text("Account")),
-        BottomNavigationBarItem(icon: Icon(Icons.bookmarks), title: Text("Recipes")),
-        BottomNavigationBarItem(icon: Icon(Icons.add_a_photo_rounded), title: Text("Posts")),
-      ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text("Account")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmarks), title: Text("Recipes")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_a_photo_rounded), title: Text("Posts")),
+        ],
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/background.png"),
-            fit: BoxFit.cover
-          ),
+            image: DecorationImage(
+                image: AssetImage("images/background.png"), fit: BoxFit.cover)),
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressIndicatorColor: Colors.blueAccent,
+          width: 400,
         ),
-        
       ),
     );
   }
 }
-
-
