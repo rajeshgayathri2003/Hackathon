3aee8c08d66492bf7774bdff705b80b0fc602834
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-10-19 11:18:14 +0530

    Navigation Bar and Image Upload

--- kitchen_tales/lib/ui/home.dart
+++ kitchen_tales/lib/ui/home.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class kitchen_tales extends StatelessWidget {
-  YoutubePlayerController _controller = YoutubePlayerController(
-      initialVideoId: '5NrYqZCjpN0',
-      flags: YoutubePlayerFlags(
-        autoPlay: false,
-        mute: false,
-      ));
-
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kitchen Tales',
+      home: MyHomePage(
+        title: 'Kitchen Tales',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -21,6 +33,7 @@ class kitchen_tales extends StatelessWidget {
         // ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.account_circle), title: Text("Account")),
@@ -28,19 +41,20 @@ class kitchen_tales extends StatelessWidget {
               icon: Icon(Icons.bookmarks), title: Text("Recipes")),
           BottomNavigationBarItem(
               icon: Icon(Icons.add_a_photo_rounded), title: Text("Posts")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.movie_filter_rounded), title: Text('Videos')),
         ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("images/background.png"), fit: BoxFit.cover)),
-        child: YoutubePlayer(
-          controller: _controller,
-          showVideoProgressIndicator: true,
-          progressIndicatorColor: Colors.blueAccent,
-          width: 400,
-        ),
       ),
     );
   }
---
+++ kitchen_tales/lib/ui/posts.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class post extends StatefulWidget {
+  @override
+  _postState createState() => _postState();
+}
+
+class _postState extends State<post> {
+  String imageUrl;
+  File imageFile;
+
+  _openGallery(BuildContext context) async {
+    imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState(() {});
+    Navigator.of(context).pop();
+  }
+
+  _openCamera(BuildContext context) async {
+    imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState(() {});
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Text('Gallery'),
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                  ),
+                  GestureDetector(
+                    child: Text('Camera'),
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _decideImageview() {
+    if (imageFile == null) {
+      return Text('No Image Selected');
+    } else {
+      return Image.file(
+        imageFile,
+        width: 400,
+        height: 400,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Kitchen Tales", style: TextStyle(color: Colors.black)),
+          centerTitle: true,
+          backgroundColor: Colors.yellowAccent,
+          // actions:
+        ),
+        body: Column(children: <Widget>[
+          (imageUrl != null)
+              ? Image.network(imageUrl)
+              : Placeholder(
+                  fallbackHeight: 200.0,
+                  fallbackWidth: double.infinity,
+                ),
+          SizedBox(
+            height: 20.0,
+          ),
+          RaisedButton(
+              child: Text('Upload Image'),
+              color: Colors.lightBlue,
+              onPressed: () {
+                _showChoiceDialog(context);
+              })
+        ]));
+  }
+}
