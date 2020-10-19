ceace0eceb3ecd022ba07f1875e343c5c5ce1d5d
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-10-19 22:19:24 +0530

    22:19

--- kitchen_tales/lib/main.dart
+++ kitchen_tales/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:kitchen_tales/ui/detail_screen.dart';
+import 'ui/recipe.dart';
+import 'ui/posts.dart';
+import 'ui/youtube.dart';
+import 'ui/home_screen.dart';
 import 'ui/home.dart';
 
 void main() {
@@ -8,4 +12,77 @@ void main() {
   ));
 }
 
-void main() => runApp(new recipe());
+recipe_screen() => runApp(new recipe());
+
+class kitchen_tales extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kitchen Tales',
+      home: MyHomePage(
+        title: 'Kitchen Tales',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final tabs = [
+    Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/background.png"), fit: BoxFit.cover)),
+    ),
+    recipe(),
+    post(),
+    player()
+  ]; //don't change the above list
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kitchen Tales", style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        backgroundColor: Colors.yellowAccent,
+        // actions: [
+        //   IconButton(icon: Icon(Icons.add_a_photo_sharp), onPressed: null)
+        // ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Colors.black,
+              icon: Icon(Icons.account_circle),
+              title: Text("Account")),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.black,
+              icon: Icon(Icons.bookmarks),
+              title: Text("Recipes")),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.black,
+              icon: Icon(Icons.add_a_photo_rounded),
+              title: Text("Posts")),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.black,
+              icon: Icon(Icons.movie_filter_rounded),
+              title: Text('Videos')),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      body: tabs[_currentIndex],
+    ); //don't change the nav bar
+  }
+}
--- kitchen_tales/lib/ui/recipe.dart
+++ kitchen_tales/lib/ui/recipe.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/material.dart';
 
 class recipe extends StatelessWidget {
   //Stateless = immutable = cannot change object's properties
@@ -7,55 +7,48 @@ class recipe extends StatelessWidget {
   Widget build(BuildContext context) {
     //Now we need multiple widgets into a parent = "Container" widget
     Widget titleSection = new Container(
-      padding: const EdgeInsets.all(10.0),//Top, Right, Bottom, Left
+      padding: const EdgeInsets.all(10.0), //Top, Right, Bottom, Left
       child: new Row(
         children: <Widget>[
           new Expanded(
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Container(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: new Text("Apple Cake",
-                        style: new TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                        )),
-                  ),
-                  //Need to add space below this Text ?
-                  new Text("Sift together the all-purpose flour and baking soda. It is done to evenly distribute the baking soda in flour. Gradually, add this to the egg mixture. If required, add a little milk and mix till the batter is fluffy and soft. You may not add the entire milk if you feel the consistency of your cake is fine and it pours down like canned condensed milk. Add vanilla essence and blend well. Vanilla essence is important to camouflage the smell of eggs.
-                  ",
-                    style: new TextStyle(
-                        color: Colors.grey[850],
-                        fontSize: 16.0
-                    ),
-                  ),
-                ],
-              ),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: new Text("Apple Cake",
+                      style: new TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18.0)),
+                ),
+                //Need to add space below this Text ?
+                new Text(
+                  "Sift together the all-purpose flour and baking soda. It is done to evenly distribute the baking soda in flour. Gradually, add this to the egg mixture. If required, add a little milk and mix till the batter is fluffy and soft. You may not add the entire milk if you feel the consistency of your cake is fine and it pours down like canned condensed milk. Add vanilla essence and blend well. Vanilla essence is important to camouflage the smell of eggs.",
+                  style: new TextStyle(color: Colors.grey[850], fontSize: 16.0),
+                ),
+              ],
+            ),
           ),
           new Icon(Icons.favorite, color: Colors.red),
-          new Text(" 100", style: new TextStyle(fontSize: 16.0),),
+          new Text(
+            " 100",
+            style: new TextStyle(fontSize: 16.0),
+          ),
         ],
       ),
     );
     //build function returns a "Widget"
     return new MaterialApp(
-      title: "",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Kitchen Tales'),
-        ),
-        body: new ListView(
-          children: <Widget>[
-            new Image.asset(
-              'images/cake.png',
-              fit: BoxFit.cover
+        title: "",
+        home: new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Kitchen Tales'),
             ),
-            //You can add more widget bellow
-            titleSection
-          ],
-        )
-      )
-    );//Widget with "Material design"
+            body: new ListView(
+              children: <Widget>[
+                new Image.asset('images/cake.png', fit: BoxFit.cover),
+                //You can add more widget bellow
+                titleSection
+              ],
+            ))); //Widget with "Material design"
   }
 }
